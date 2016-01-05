278490d3ca1aa0a2087db5df8a461c1998f398d9
MassD <massd.me@gmail.com>
2016-01-05 06:48:36 -0500

    corrected boundary inclusive version

--- pearl_4_double_binary_search/kth_union.ml
+++ pearl_4_double_binary_search/kth_union.ml
@@ -18,16 +18,16 @@ let kth_2_arrays k a b =
 let kth_union k a b = 
   let sa = Array.length a and sb = Array.length b in
   let rec kth k (a, la, ha) (b, lb, hb) =
-    if la >= ha then b.(k+lb-1)
-    else if lb >= hb then a.(k+la-1)
+    if la >= ha+1 then b.(k+lb)
+    else if lb >= hb+1 then a.(k+la)
     else  
       let ma = (ha+la)/2 and mb = (hb+lb)/2 in
       match a.(ma) < b.(mb), k >= ma-la+1+mb-lb with
       | true, true -> kth (k-(ma-la+1)) (a, ma+1, ha) (b, lb, hb)
-      | true, false -> kth k (a, la, ha) (b, lb, mb)
+      | true, false -> kth k (a, la, ha) (b, lb, mb-1)
       | _ -> kth k (b, lb, hb) (a, la, ha)
   in 
-  kth k (a, 0, sa) (b, 0, sb)
+  kth k (a, 0, sa-1) (b, 0, sb-1)
 
 let a = [|3;7;16;25;26|]
 let b = [|1;5;13;28;30;36;51;58|]
